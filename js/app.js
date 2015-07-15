8d0810b918b8d349b51567b88ac5cc9c973687d9
Jared Hensley <jahensle85@me.com>
2015-07-15 19:49:00 -0400

    fix

--- js/app.js
+++ js/app.js
@@ -14,11 +14,13 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').show();
+		//shows his ready state and hadouken 
 	});
 	$('.ryu').mouseup(function(){
 		 console.log('test test');
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
+		//return ryu to still state
 	});
 
 
