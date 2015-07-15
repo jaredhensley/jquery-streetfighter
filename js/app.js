fb8a5776ec11b3f190adeadf49c4220b736381cd
Jared Hensley <jahensle85@me.com>
2015-07-15 19:55:24 -0400

    fix again

--- js/app.js
+++ js/app.js
@@ -5,16 +5,19 @@ $(document).ready(function() {
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
 	});
+	//first event, mouse hovers and ryu is rdy
 	$('.ryu').mouseleave(function () {
 		$(".ryu-ready").hide();
 		$(".ryu-still").show();
 		});
+	//second event, move mouse away and ryu rests
 	$('.ryu').mousedown(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').show();
 	});
+	//third event, click mouse and ryu throws and hadouken appears
 	$('.ryu').mouseup(function(){
 		 console.log('test test');
 		$('.ryu-throwing').hide();
